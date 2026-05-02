eb3c285e03e11f7283efc3408eddd8ef135b54dc
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-02 10:16:08 +0900

    カッコのミス

--- UniQUE-Front/next.config.ts
+++ UniQUE-Front/next.config.ts
@@ -24,15 +24,14 @@ const nextConfig: NextConfig = {
             value: "strict-origin-when-cross-origin",
           },
           {
-           {
-             key: "Content-Security-Policy",
-             value:
-               process.env.NODE_ENV === "production"
-                 ? "default-src 'self'; base-uri 'self'; frame-ancestors 'none'; object-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
-                 : "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'",
-           },
+            key: "Content-Security-Policy",
+            value:
+              process.env.NODE_ENV === "production"
+                ? "default-src 'self'; base-uri 'self'; frame-ancestors 'none'; object-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
+                : "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'",
+          },
         ],
-      }
+      },
     ];
   },
 };
